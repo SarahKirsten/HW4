02b525557ef361df0a19d3944b371f4e77228365
SarahKirsten <deranged.link@gmail.com>
2016-10-18 18:58:15 -0700

    FINISHED EX04_01

--- EX04_01/EX04_01/Header.h
+++ EX04_01/EX04_01/Header.h
@@ -1,1 +1,24 @@
+/////////////////////////
+//Program Name: Liang 11.1
+//Name: Sarah Kirsten
+//Class and Section: CS172-1
+//Date: 10/18/2016
+///////////////////////
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+int * alloc_array(int size)
+{
+	int* printarray = new int[size];
+
+	return printarray;
+}
+
 #pragma once
---
+++ EX04_01/EX04_01/Source.cpp
@@ -0,0 +1,61 @@
+/////////////////////////
+//Program Name: Liang 11.1
+//Name: Sarah Kirsten
+//Class and Section: CS172-1
+//Date: 10/18/2016
+///////////////////////
+
+#include "Header.h"
+
+
+
+int main()
+{
+	//size of array
+	cout << "Enter the size of the array: ";
+	int s;
+	cin >> s;
+	cout << endl;
+
+	//sets size of array into function
+	int* array = alloc_array(s);
+
+	//reads in values for array
+	for (int i = 0; i < s;i++)
+	{
+		cout << "Enter an array value: ";
+		cin >> array[i];
+	}
+	cout << endl;
+
+	//read out the array
+	//for (int i = 0; i < s; i++)
+	//	cout << array[i] << endl;
+	//cout << endl;
+
+	//compute sum and then average of values in array
+	int sum = 0;
+	int avg = 0;
+	for (int i = 0; i < s;i++)//sum of array values
+	{
+		sum += array[i];
+	}
+	avg = sum / s;//average of array values
+	cout << "Average is: " << avg << endl;
+	cout << endl;
+
+	int count = 0;
+	for (int i = 0;i < s;i++)//how many values above the average
+	{
+		
+		if (array[i] >= avg)
+			count++;
+	}
+	cout << "Number of values above the average: " << count << endl << endl;
+
+
+
+
+	delete[] array;
+	return 0;
+}
